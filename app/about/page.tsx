f9d6cec885bc15baf9aacea4529cdfdc66021ed4
Khalaf <joao.khalaf@gmail.com>
2024-09-02 19:16:32 -0300

    add gradiants, fixing about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,31 +2,30 @@ import Image from "next/image";
 
 export default function AboutPage() {
   return (
-    <article className="flex flex-col justify-center items-center mx-auto max-w-prose min-h-[90vh] px-4">
-      <div className="flex justify-center items-center mb-16">
+    <article className="container mx-auto max-w-prose flex flex-col justify-center items-center px-4 py-32 mt-3">
+      <div className="flex justify-center items-center mb-14">
         <Image
           src={"/images/author/meme.png"}
           alt={"Garfield meme dizendo que o café não é forte o bastante, feito por Paws."}
-          width={350}
+          width={650}
           height={50}
-          className="object-cover rounded-3xl w-full max-w-xs"
+          className="object-cover rounded-3xl w-full max-w-sm"
         />
       </div>
-      
+
       <div className="text-center">
-        <p className="py-2 text-base sm:text-lg">
-          Sou um engenheiro de software brasileiro apaixonado por criar coisas
-          do zero. Atualmente, estou cursando uma graduação em sistemas de internet na
+        <p className="py-2 sm:py-4 text-base sm:text-lg">
+          Sou um engenheiro de software, gosto de criar coisas que possam ajudar tanto o meu dia a dia quanto de outras pessoas. Atualmente, estou cursando graduação em sistemas de internet na
           Fatec Rubens Lara, Brasil.
         </p>
         <p className="py-2 text-base sm:text-lg">
-          Gosto de academia, música, boa comida, anime e café (bastante café). Sou uma pessoa simples, não espero muito de mim, mas vejo meu valor.
+          Gosto de academia, música, anime e café (bastante café). Sou uma pessoa simples, não espero muito de mim, mas vejo meu valor.
         </p>
-        <p className="py-2 text-base sm:text-lg">
+        <p className="py-2 sm:py-4 text-base sm:text-lg">
           Obrigado por visitar!
           Espero que tenha gostado do que viu.
           Sinta-se à vontade para entrar em contato a qualquer momento.
-          Vamos criar algo incrível juntos!
+          Vamos criar algo!
         </p>
       </div>
     </article>
--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -7,7 +7,7 @@ export default async function PostPage() {
   return (
     <section className="py-40">
       <div className="container max-w-4xl">
-        <p className='title mb-12'> Boa parte dos posts são anotações dos meus estudos tiradas do Obsidian.</p>
+        <p className='title mb-12'> Os posts são anotações dos meus estudos tiradas do Obsidian.</p>
         <h1 className="title mb-12"> Posts </h1>
 
         <Posts posts={posts} />
